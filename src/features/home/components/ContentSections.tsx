5aca1610f2bf315b8659db1f319d6e98feb9dd05
JordyTshibssB <169159357+JordyTshibssB@users.noreply.github.com>
2026-06-02 21:41:27 +0400

    fix: content sections

--- src/features/home/components/ContentSections.tsx
+++ src/features/home/components/ContentSections.tsx
@@ -5,22 +5,19 @@ import { Card, CardContent } from "@/shared/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 
-const SECTION_IMAGES = [
-  "https://thumbs.dreamstime.com/b/property-news-information-updates-related-to-real-estate-market-text-concept-background-362539602.jpg",
-  "https://thumbs.dreamstime.com/z/brown-surface-house-pen-notepad-inscription-property-news-business-concept-brown-surface-248951635.jpg",
-];
+
 
 export default function ContentSections() {
   const t = useT();
 
   const sections = [
     {
-      image: SECTION_IMAGES[0],
+      image: "/assets/images/article-1.jpg",
       title: t.home.content.card1Title,
       excerpt: t.home.content.card1Excerpt,
     },
     {
-      image: SECTION_IMAGES[1],
+      image: "/assets/images/article-2.jpg",
       title: t.home.content.card2Title,
       excerpt: t.home.content.card2Excerpt,
     },
@@ -36,20 +33,14 @@ export default function ContentSections() {
           {t.home.content.heading}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {sections.map((section, index) => (
+          {sections.map((section,index) => (
             <Link href="/blog" key={index}>
               <Card
                 key={section.title}
                 className="rounded-xl shadow-md overflow-hidden border border-border"
               >
-                <div className="w-full h-64 bg-muted">
-                  <Image
-                    src={section.image}
-                    alt={section.title}
-                    width={100}
-                    height={48}
-                    className="w-full h-10/9 object-cover"
-                  />
+                <div className="w-full h-128 bg-muted">
+                  <Image src={section.image} alt={section.title} width={580} height={8} className="object-cover h-full"/>
                 </div>
 
                 <CardContent className="p-6 mt-8">
